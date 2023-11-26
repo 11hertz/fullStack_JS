3eff13e002c687f4f78a75dfb875e61994f125a0
11hertz <reachzin@naver.com>
2023-11-26 19:52:33 +0900

    add modPriority to Operator

--- TryThis/Operator/modPriority.js
+++ TryThis/Operator/modPriority.js
@@ -2,3 +2,15 @@
 // % 우선순위는 +- 보다는 앞선다.
 // % 우선순위는 */ 와는 같다.
 // ** 우선순위는 % 보다 앞선다.
+
+const a = 10;
+const b = 5;
+const c = 3;
+
+const res1 = a + b % c === (a + b) % c; // false - %가 우선순위 높음
+const res2 = a * b % c === (a * b) % c; // true - 우선순위가 같아서 순서대로 실행
+const res3 = a ** b % c === (a ** b) % c; // true - **가 우선순위 높아서 순서대로 실행
+
+console.log(res1);
+console.log(res2);
+console.log(res3);
